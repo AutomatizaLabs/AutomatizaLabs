5b261e0483d237df8b3f8c6cf9e94515c1fd8e44
Vidotto <vitotbrbr@gmail.com>
2024-03-20 21:59:18 -0300

    Chore: Home Page and Layout commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import ResponsiveAppBar from "@/Components/Appbar";
+import HomePage from "@/Components/HomePage";
+
+export default function Home() {
+  return (
+    <main>
+      <ResponsiveAppBar />
+      <HomePage />
+    </main>
+  );
+}
