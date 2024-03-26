61b70c4b4a7a0f140258273cda6a6900b7b7161c
Vidotto <vitotbrbr@gmail.com>
2024-03-26 20:22:14 -0300

    chore : services avaliable

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -10,7 +10,7 @@ const projectsData = [
     title: "React Portfolio Website",
     description: "Project 1 description",
     image: "/images/projects/1.png",
-    tag: ["All", "Web"],
+    tag: ["Todos", "Web"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -19,7 +19,7 @@ const projectsData = [
     title: "Potography Portfolio Website",
     description: "Project 2 description",
     image: "/images/projects/2.png",
-    tag: ["All", "Web"],
+    tag: ["Todos", "Web"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -28,7 +28,7 @@ const projectsData = [
     title: "E-commerce Application",
     description: "Project 3 description",
     image: "/images/projects/3.png",
-    tag: ["All", "Web"],
+    tag: ["Todos", "Web"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -37,7 +37,7 @@ const projectsData = [
     title: "Food Ordering Application",
     description: "Project 4 description",
     image: "/images/projects/4.png",
-    tag: ["All", "Mobile"],
+    tag: ["Todos", "Celular"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -46,7 +46,7 @@ const projectsData = [
     title: "React Firebase Template",
     description: "Authentication and CRUD operations",
     image: "/images/projects/5.png",
-    tag: ["All", "Web"],
+    tag: ["Todos", "Web"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -55,14 +55,14 @@ const projectsData = [
     title: "Full-stack Roadmap",
     description: "Project 5 description",
     image: "/images/projects/6.png",
-    tag: ["All", "Web"],
+    tag: ["Todos", "Web"],
     gitUrl: "/",
     previewUrl: "/",
   },
 ];
 
 const ProjectsSection = () => {
-  const [tag, setTag] = useState("All");
+  const [tag, setTag] = useState("Todos");
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
 
@@ -88,7 +88,7 @@ const ProjectsSection = () => {
         <ProjectTag
           onClick={handleTagChange}
           name="Todos"
-          isSelected={tag === "All"}
+          isSelected={tag === "Todos"}
         />
         <ProjectTag
           onClick={handleTagChange}
@@ -98,7 +98,7 @@ const ProjectsSection = () => {
         <ProjectTag
           onClick={handleTagChange}
           name="Celular"
-          isSelected={tag === "Mobile"}
+          isSelected={tag === "Celular"}
         />
       </div>
       <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
