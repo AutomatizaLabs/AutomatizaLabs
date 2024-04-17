ea713338c755bdbe49da25a901c31aadf9ede083
Vidotto <vitotbrbr@gmail.com>
2024-04-17 20:18:41 -0300

    Feature: route protection complete working

---
+++ hook/addUser.js
@@ -0,0 +1,61 @@
+import { db } from "../firebase";
+import {
+  collection,
+  addDoc,
+  updateDoc,
+  doc,
+  deleteDoc,
+  getDocs,
+  getDoc,
+  setDoc,
+} from "firebase/firestore";
+
+// const updateConfig = async (docId, updatedData) => {
+//   try {
+//     const configRef = doc(db, "users", docId);
+//     await updateDoc(configRef, updatedData);
+//   } catch (error) {
+//     console.error("Error updating config:", error);
+//     throw error;
+//   }
+// };
+
+const addConfig = async ({ userId, userRole }) => {
+  try {
+    const userDocRef = doc(db, "users", userId);
+    await setDoc(userDocRef, { userRole: String(userRole) });
+    console.log("Funcionou");
+  } catch (error) {
+    console.error("Error adding user config:", error);
+    throw error;
+  }
+};
+const checkUserExistence = async (userId) => {
+  try {
+    const userDocRef = doc(db, "users", userId);
+    const userDocSnap = await getDoc(userDocRef);
+
+    return userDocSnap.exists(); // Retorna true se o usuário existir, false caso contrário
+  } catch (error) {
+    console.error("Error checking user existence:", error);
+    throw error;
+  }
+};
+const getUserRole = async (userId) => {
+  try {
+    const userDocRef = doc(db, "users", userId);
+    const userDocSnap = await getDoc(userDocRef);
+
+    if (userDocSnap.exists()) {
+      const userData = userDocSnap.data();
+      return userData.userRole; // Retorna a função do usuário se o documento existir
+    } else {
+      return null; // Retorna null se o documento do usuário não existir
+    }
+  } catch (error) {
+    console.error("Error getting user role:", error);
+    throw error;
+  }
+};
+  
+  export { addConfig, checkUserExistence, getUserRole };
--- hook/useAuth.js
+++ hook/useAuth.js
@@ -1,23 +1,46 @@
-'use client'
 import { useEffect, useState } from "react";
-import { auth } from "../firebase";
-import { useRouter } from 'next/navigation'
+import { auth, db } from "../firebase"; // Assumindo que 'db' é a instância do Firestore que você está usando
+import { useRouter } from 'next/navigation'; // Corrigi o import para 'router' em vez de 'navigation'
+import { addConfig, checkUserExistence, getUserRole } from "./addUser";
+
 const useAuth = () => {
     const [user, setUser] = useState(null);
     const [redirected, setRedirected] = useState(false);
     const router = useRouter();
 
     useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-            setUser(user);
-            if (!user && !redirected) {
+        const unsubscribe = auth.onAuthStateChanged(async (authUser) => {
+            setUser(authUser);
+            if (!authUser && !redirected) {
                 setRedirected(true);
-                router.push("/admin");
+                router.push("/login");
+            } else {
+                try {
+                    const configExists = await checkUserExistence(authUser.uid);
+                    if(!configExists){
+
+                        await addConfig({
+                            userId: authUser.uid, 
+                            userRole: "user"
+                        });
+                        return
+                    }
+                    const userRole = await getUserRole(authUser.uid);
+                    if (userRole === "admin") {
+                        router.push("/adminpanel");
+                    } else {
+                        router.push("/userpanel");
+                    }
+
+                } catch (error) {
+                    console.error("Error adding config:", error);
+                }
             }
         });
 
         return () => unsubscribe();
     }, [redirected, router]);
+
     return { user };
 };
 
