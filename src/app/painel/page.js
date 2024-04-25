8bdc4c03919d3e9d5f6ffde5c3068f2c1f0187ab
Vidotto <vitotbrbr@gmail.com>
2024-04-25 17:34:58 -0300

    chore: painel admin working

--- src/app/painel/page.js
+++ src/app/painel/page.js
@@ -8,6 +8,10 @@ import useAuth from "../../../hook/useAuth";
 import Auth from '../components/Auth';
 import PainelUserNavBar from '../components/PainelUserNavBar';
 import PainelAdminNavBar from '../components/PainelAdmNavBar';
+import AddTodo from '../components/AddTodo';
+import { Box, Container, Flex } from '@chakra-ui/react';
+import TodoListedit from '../components/TodolistEditor';
+import ModalContextProvider from '../contexts/ModalContext';
 
 export default function UserPanel() {
   const { user, isAdmin } = useAuth();
@@ -18,13 +22,20 @@ export default function UserPanel() {
   }
 
   return (
+    
+    <>
     <main className="flex min-h-screen flex-col bg-[#ffffff]">
+      <ModalContextProvider >
       {isAdmin ? <PainelAdminNavBar /> : <PainelUserNavBar />}
       <div className="container mt-24 mx-auto px-12 py-4">
+      {isAdmin ? <AddTodo /> : <p/>}
+      {isAdmin ? <TodoListedit />: <p/>}
         <ProjectsSection />
         <EmailSection />
       </div>
       <Footer />
+    </ModalContextProvider>
     </main>
+    </>
   );
 }
